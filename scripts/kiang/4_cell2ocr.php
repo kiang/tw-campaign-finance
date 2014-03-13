40f31adf0f264854685fe676d52e784e0ca0bfec
kiang <kiange@gmail.com>
2014-03-13 20:57:59 +0800

    cache downloaded files and set different language based on columns

--- scripts/kiang/4_cell2ocr.php
+++ scripts/kiang/4_cell2ocr.php
@@ -1,8 +1,8 @@
 <?php
 
-$path = dirname(dirname(__FILE__));
+$path = dirname(dirname(__DIR__));
 
-$oh = fopen($path . '/output.csv', 'r');
+$oh = fopen($path . '/output2.csv', 'r');
 $firstLineSkipped = false;
 while ($oFile = fgetcsv($oh, 512)) {
     /*
@@ -22,12 +22,23 @@ while ($oFile = fgetcsv($oh, 512)) {
     }
     $qPos = strrpos($oJson->url, '.');
     $fileType = substr($oJson->url, $qPos + 1, 3);
+    $fileCache = $path . '/tmp/' . md5($oJson->url) . '.' . $fileType;
+    $fileCacheFolder = substr($fileCache, 0, -32);
+    if (!file_exists($fileCacheFolder)) {
+        mkdir($fileCacheFolder, 0777, true);
+    }
+    $fileCache = $fileCacheFolder . '/' . substr($fileCache, -32);
+    if (!file_exists($fileCache)) {
+        file_put_contents($fileCache, file_get_contents($oJson->url));
+        exec("convert {$fileCache} -morphology thicken '1x3>:1,0,1' {$fileCache}");
+        exec("convert {$fileCache} -morphology thicken '1x3>:1,0,1' {$fileCache}");
+    }
     switch ($fileType) {
         case 'jpg':
-            $img = imagecreatefromjpeg($oJson->url);
+            $img = imagecreatefromjpeg($fileCache);
             break;
         case 'png':
-            $img = imagecreatefrompng($oJson->url);
+            $img = imagecreatefrompng($fileCache);
             break;
         default:
             die(">>> {$oJson->url} <<<;");
@@ -39,15 +50,28 @@ while ($oFile = fgetcsv($oh, 512)) {
             foreach ($line AS $y => $cell) {
                 $cell = (array) $cell;
                 $croppedImg = imagecrop($img, $cell);
+                switch ($y) {
+                    case 1:
+                    case 2:
+                    case 5:
+                    case 6:
+                    case 7:
+                        $lang = 'eng';
+                        break;
+                    default:
+                        $lang = 'chi_tra';
+                }
+
+
                 if (false !== $croppedImg) {
                     switch ($fileType) {
                         case 'jpg':
                             imagejpeg($croppedImg, "{$path}/scripts/good.jpg");
-                            exec("/usr/bin/tesseract {$path}/scripts/good.jpg /tmp/u -l chi_tra");
+                            exec("/usr/bin/tesseract {$path}/scripts/good.jpg /tmp/u -l {$lang}");
                             break;
                         case 'png':
                             imagepng($croppedImg, "{$path}/scripts/good.png");
-                            exec("/usr/bin/tesseract {$path}/scripts/good.png /tmp/u -l chi_tra");
+                            exec("/usr/bin/tesseract {$path}/scripts/good.png /tmp/u -l {$lang}");
                             break;
                     }
                     if (filesize("/tmp/u.txt") > 0) {
