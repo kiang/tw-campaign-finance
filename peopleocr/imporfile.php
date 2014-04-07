ee95a2df7464c8e92f40a6a1260e892f8439fcb8
ojelly <jelly@ojelly.com>
2014-04-07 08:45:33 +0800

    edit db table

--- peopleocr/imporfile.php
+++ peopleocr/imporfile.php
@@ -12,6 +12,42 @@
 
     //開啟清單檔案
     $fp = fopen($path_list , "r");
+
+
+    /*
+    
+    CREATE TABLE IF NOT EXISTS `pocr_filestatus` (
+      `filSno` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主鍵',
+      `filName` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '檔名',
+      `filPage` int(10) unsigned DEFAULT '0' COMMENT '頁數',
+      `filImage` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '圖檔位置',
+      `filImagewidth` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '圖片寬度',
+      `filImageheight` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '圖片高度',
+      `filCanvas` text COLLATE utf8_unicode_ci COMMENT 'Canvas',
+      `filStatusedit` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '編輯狀態{''0'':''禁止編輯'',''1'':''可編輯'',''7'':''編輯中''}',
+      `filStatusocr` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '分析狀態{''0'':''待驗證'',''1'':''驗證完成''}',
+      `filSessionid` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '編輯中',
+      `filTimeedit` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新時間',
+      PRIMARY KEY (`filSno`)
+    ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='檔案清單' AUTO_INCREMENT=1 ;
+
+
+    CREATE TABLE IF NOT EXISTS `pocr_filefields` (
+      `fidSno` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主鍵',
+      `filSno` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '檔案主鍵',
+      `fidRow` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '列',
+      `fidCol` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '欄',
+      `fidValue` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '欄位值',
+      `fidStatus` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '狀態 {''0'':''未校對'',''1'':''已校對'',''7'':''待驗證''}',
+      `fidTimeedit` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新時間',
+      PRIMARY KEY (`fidSno`),
+      KEY `filSno` (`filSno`,`fidRow`,`fidCol`)
+    ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='欄位值' AUTO_INCREMENT=1 ;
+
+    */
+
+
+
     
     //寫入資料庫
     $i = 0;
