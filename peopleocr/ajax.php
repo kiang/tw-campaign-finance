a9a5f24a9d1abdcbc80365bbfc90e96d0287b6ea
jelly@ux32 <jelly@ojelly.com>
2014-04-19 11:52:12 +0800

    add api to get all page field

--- peopleocr/ajax.php
+++ peopleocr/ajax.php
@@ -2,7 +2,7 @@
     include_once("./_config/config.php");
     $rep = returnpost($_POST);
 
-   if($_POST['act'] == "save_field"){
+    if($_POST['act'] == "save_field"){
         $fileds = $rep['data']['data'][0];
         foreach ($fileds as $key => $filed) {
             print_r($filed);
@@ -20,6 +20,19 @@
 
             myquery($sql);
         }
+    }
+    if($_GET['act'] == "getdata"){
+        $data = array();
+        $rep = returnpost($_GET);
+        if(!empty($rep['id'])){
+            $data['sql'] = " select fidRow,fidCol,fidValue from ".__table_prefix."filefields where filSno = '".$rep['id']."' order by fidRow , fidCol ";
+            $res = myquery($data['sql']);
+            while($row = mysql_fetch_assoc($res)){
+                $data[$row['fidRow']][$row['fidCol']] = $row['fidValue'];
+            }
+        }
+        unset($data['sql']);
+        echo json_encode($data);
     }
 
 
