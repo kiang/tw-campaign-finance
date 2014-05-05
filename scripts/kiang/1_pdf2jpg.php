d599c85e6ea0172591ca5cd1318c5aed44952d6d
kiang <kiange@gmail.com>
2014-05-05 10:46:20 +0800

    fix fh position

--- scripts/kiang/1_pdf2jpg.php
+++ scripts/kiang/1_pdf2jpg.php
@@ -17,6 +17,11 @@ if (!file_exists($pdfImgPath)) {
     mkdir($pdfImgPath, 0777, true);
 }
 
+$fh = fopen($path . '/pdf/pdf2jpg.csv', 'w');
+fputcsv($fh, array('id', '檔名', '頁數', '網址', '圖寬', '圖高'));
+$fileId = 0;
+$pageNumbers = array();
+
 $rotateMap = array();
 $rotateMapFile = $path . '/pdf/rotateMap.csv';
 if (file_exists($rotateMapFile)) {
@@ -28,54 +33,6 @@ if (file_exists($rotateMapFile)) {
     }
 }
 
-$fh = fopen($path . '/pdf/pdf2jpg.csv', 'w');
-fputcsv($fh, array('id', '檔名', '頁數', '網址', '圖寬', '圖高'));
-$fileId = 0;
-$pageNumbers = array();
-foreach (glob($path . '/pdf/src/*/*.pdf') AS $file) {
-    $pdfPath = substr($file, $pathLength + 5);
-    $fileToken = md5($pdfPath);
-    $file = str_replace(array(' ', '(', ')'), array('\\ ', '\\(', '\\)'), $file);
-    if (!file_exists("{$pdfImgPath}/{$fileToken}-0001.jpg")) {
-        error_log("Extracting images from {$file}");
-        exec("gs -dNOPAUSE -dNumRenderingThreads=4 -sDEVICE=jpeg -sOutputFile={$fileToken}-%04d.jpg -dJPEGQ=90 -r300x300 -q {$file} -c quit");
-        foreach (glob("{$path}/{$fileToken}-*") AS $jpg) {
-            $dashPos = strrpos($jpg, '-');
-            $dotPos = strpos($jpg, '.', $dashPos);
-            $pageNumber = substr($jpg, $dashPos + 1, $dotPos - $dashPos - 1);
-            //copy($jpg, "{$pdfImgPath}/{$fileToken}-{$pageNumber}.jpg");
-            exec("convert -morphology thicken '1x3>:1,0,1' -threshold 70% {$jpg} {$jpg}");
-            $size = getimagesize($jpg);
-            if ($size[0] < $size[1]) {
-                $source = imagecreatefromjpeg($jpg);
-                $rotate = imagerotate($source, 270, 0);
-                imagejpeg($rotate, $jpg);
-                $tmp = $size[1];
-                $size[1] = $size[0];
-                $size[0] = $tmp;
-            }
-            exec("mv {$jpg} {$pdfImgPath}/{$fileToken}-{$pageNumber}.jpg");
-            fputcsv($fh, array(++$fileId, $pdfPath, $pageNumber, "img_orig/{$fileToken}-{$pageNumber}.jpg", $size[0], $size[1]));
-        }
-        error_log("Finished extracting images from {$file}");
-    } else {
-        foreach (glob("{$pdfImgPath}/{$fileToken}-*") AS $jpg) {
-            $dashPos = strrpos($jpg, '-');
-            $dotPos = strpos($jpg, '.', $dashPos);
-            $pageNumber = substr($jpg, $dashPos + 1, $dotPos - $dashPos - 1);
-            $size = getimagesize($jpg);
-            if ($size[0] < $size[1]) {
-                $source = imagecreatefromjpeg($jpg);
-                $rotate = imagerotate($source, 270, 0);
-                imagejpeg($rotate, $jpg);
-                $tmp = $size[1];
-                $size[1] = $size[0];
-                $size[0] = $tmp;
-            }
-            fputcsv($fh, array(++$fileId, substr($file, 48), $pageNumber, "img_orig/{$fileToken}-{$pageNumber}.jpg", $size[0], $size[1]));
-        }
-    }
-}
 //width = 3525
 foreach (glob($path . '/pdf/src/*/*.tif') AS $file) {
     $csvFilePath = substr($file, 48);
@@ -132,5 +89,51 @@ foreach (glob($path . '/pdf/src/*/*.jpg') AS $file) {
     }
     fputcsv($fh, array(++$fileId, $csvFilePath, $pageNumber, "img_orig/{$fileToken}-{$pageNumber}.jpg", $size[0], $size[1]));
 }
+
+foreach (glob($path . '/pdf/src/*/*.pdf') AS $file) {
+    $pdfPath = substr($file, $pathLength + 5);
+    $fileToken = md5($pdfPath);
+    $file = str_replace(array(' ', '(', ')'), array('\\ ', '\\(', '\\)'), $file);
+    if (!file_exists("{$pdfImgPath}/{$fileToken}-0001.jpg")) {
+        error_log("Extracting images from {$file}");
+        exec("gs -dNOPAUSE -dNumRenderingThreads=4 -sDEVICE=jpeg -sOutputFile={$fileToken}-%04d.jpg -dJPEGQ=90 -r300x300 -q {$file} -c quit");
+        foreach (glob("{$path}/{$fileToken}-*") AS $jpg) {
+            $dashPos = strrpos($jpg, '-');
+            $dotPos = strpos($jpg, '.', $dashPos);
+            $pageNumber = substr($jpg, $dashPos + 1, $dotPos - $dashPos - 1);
+            //copy($jpg, "{$pdfImgPath}/{$fileToken}-{$pageNumber}.jpg");
+            exec("convert -morphology thicken '1x3>:1,0,1' -threshold 70% {$jpg} {$jpg}");
+            $size = getimagesize($jpg);
+            if ($size[0] < $size[1]) {
+                $source = imagecreatefromjpeg($jpg);
+                $rotate = imagerotate($source, 270, 0);
+                imagejpeg($rotate, $jpg);
+                $tmp = $size[1];
+                $size[1] = $size[0];
+                $size[0] = $tmp;
+            }
+            exec("mv {$jpg} {$pdfImgPath}/{$fileToken}-{$pageNumber}.jpg");
+            fputcsv($fh, array(++$fileId, $pdfPath, $pageNumber, "img_orig/{$fileToken}-{$pageNumber}.jpg", $size[0], $size[1]));
+        }
+        error_log("Finished extracting images from {$file}");
+    } else {
+        foreach (glob("{$pdfImgPath}/{$fileToken}-*") AS $jpg) {
+            $dashPos = strrpos($jpg, '-');
+            $dotPos = strpos($jpg, '.', $dashPos);
+            $pageNumber = substr($jpg, $dashPos + 1, $dotPos - $dashPos - 1);
+            $size = getimagesize($jpg);
+            if ($size[0] < $size[1]) {
+                $source = imagecreatefromjpeg($jpg);
+                $rotate = imagerotate($source, 270, 0);
+                imagejpeg($rotate, $jpg);
+                $tmp = $size[1];
+                $size[1] = $size[0];
+                $size[0] = $tmp;
+            }
+            fputcsv($fh, array(++$fileId, substr($file, 48), $pageNumber, "img_orig/{$fileToken}-{$pageNumber}.jpg", $size[0], $size[1]));
+        }
+    }
+}
+
 
 fclose($fh);
